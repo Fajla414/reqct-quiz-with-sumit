6b50b3d477bf5b88bc87ff524265dd4a043758e7
Fajla Rabby <blfajlarabby414@gmail.com>
2024-10-26 07:58:22 +0600

    source code

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../styles/App.css";
 import Layout from "./Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
-        {/* <SignUp /> */}
-        {/* <Login /> */}
-        {/* <Quiz /> */}
-        <Result />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/result" element={<Result />} />
+          <Route exact path="/quiz" element={<Quiz />} />
+        </Routes>
       </Layout>
     </BrowserRouter>
   );
